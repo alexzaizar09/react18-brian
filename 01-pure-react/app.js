50b425503e70795762ea49c3a581460ac7c70ba1
Alejandro Zaizar <alexzaizar09@gmail.com>
2023-12-03 13:10:36 -0600

    setup prettier just add prettierrc file

--- 01-pure-react/app.js
+++ 01-pure-react/app.js
@@ -1,25 +1,21 @@
-const Pet = ( props ) => {
-  return React.createElement(
-    "div",
-    {},
-    [
-      React.createElement("h1", {}, props.name),
-      React.createElement("h2", {}, props.type),
-      React.createElement("h3", {}, props.breed)
-    ]
-  )
-}
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.type),
+    React.createElement("h3", {}, props.breed),
+  ]);
+};
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    [
-      React.createElement("h1", {}, "Pure React v18"),
-      React.createElement(Pet, { name: "Maia", type: "Dog", breed: "Beagle" }),
-      React.createElement(Pet, { name: "Pepper", type: "Bird", breed: "Cockatiel" }),
-      React.createElement(Pet, { name: "Doink", type: "Cat", breed: "Mixed" }),
-    ]
-  )
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Pure React v18"),
+    React.createElement(Pet, { name: "Maia", type: "Dog", breed: "Beagle" }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      type: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, { name: "Doink", type: "Cat", breed: "Mixed" }),
+  ]);
 };
 
 const container = document.getElementById("root");
