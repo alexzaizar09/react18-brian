28a5570b548cab7ca8b5ea30e68d2a5f20459043
Alejandro Zaizar <alexzaizar09@gmail.com>
2023-12-07 12:49:40 -0600

    adding react query

--- app/App.jsx
+++ app/App.jsx
@@ -2,17 +2,29 @@ import ReactDOM from "react-dom/client";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
     <BrowserRouter>
-      <header>
-        <Link to={"/"}>Adopt Me!</Link>
-      </header>
-      <Routes>
-        <Route path="/" element={<SearchParams />} />
-        <Route path="/details/:id" element={<Details />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to={"/"}>Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/" element={<SearchParams />} />
+          <Route path="/details/:id" element={<Details />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
--- app/Details.jsx
+++ app/Details.jsx
@@ -1,8 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
+import fetchPet from "./fetchPet";
 
 const Details = () => {
   const { id } = useParams();
-  return <h2>{id}</h2>;
+
+  const petRequest = useQuery({
+    queryKey: ["details", id],
+    queryFn: fetchPet,
+  });
+
+  if (petRequest.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">🌀</h2>
+      </div>
+    );
+  }
+
+  const pet = petRequest.data.pets[0];
+
+  return (
+    <div className="details">
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>
+          {pet.animal} - {pet.breed}
+        </h2>
+        <p>{pet.description}</p>
+        <button>Adopt {pet.name}</button>
+      </div>
+    </div>
+  );
 };
 
 export default Details;
