a5f94323395ada63db484c5e7a99b5297c5746ed
Alejandro Zaizar <alexzaizar09@gmail.com>
2023-12-07 11:50:18 -0600

    adding routes and context with BrowserRouter

--- app/App.jsx
+++ app/App.jsx
@@ -1,12 +1,19 @@
 import ReactDOM from "react-dom/client";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
-    <div>
-      <h1>Pure React v18</h1>
-      <SearchParams />
-    </div>
+    <BrowserRouter>
+      <header>
+        <Link to={"/"}>Adopt Me!</Link>
+      </header>
+      <Routes>
+        <Route path="/" element={<SearchParams />} />
+        <Route path="/details/:id" element={<Details />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
--- app/PetsList.jsx
+++ app/PetsList.jsx
@@ -12,6 +12,7 @@ const PetsList = ({ pets }) => {
             animal={pet.animal}
             breed={pet.breed}
             key={pet.id}
+            id={pet.id}
             images={pet.images}
             location={`${pet.city}, ${pet.state}`}
           />
